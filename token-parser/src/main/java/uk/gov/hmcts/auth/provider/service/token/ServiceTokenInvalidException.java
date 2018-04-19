dffbdb6e5574b30b07726e3e6acb38896beb7ff3
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2018-04-19 10:24:57 +0100

    Remove token parser modules (#22)

--- token-parser/src/main/java/uk/gov/hmcts/auth/provider/service/token/ServiceTokenInvalidException.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.auth.provider.service.token;
-
-/**
- * SonarQube reports as error. Max allowed - 5 parents
- */
-@SuppressWarnings("squid:MaximumInheritanceDepth")
-public class ServiceTokenInvalidException extends ServiceTokenParsingException {
-}
--- token-parser/src/main/java/uk/gov/hmcts/auth/provider/service/token/ServiceTokenParser.java
+++
@@ -1,5 +0,0 @@
-package uk.gov.hmcts.auth.provider.service.token;
-
-public interface ServiceTokenParser {
-    String parse(String jwt) throws ServiceTokenParsingException;
-}
