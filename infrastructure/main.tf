81cebfc1d3bd1b5c6ff7d25db048ba174bf98b22
Rafal Kondratowicz <rkondr@gmail.com>
2018-04-23 16:56:14 +0100

    Retrieven resource group name from api module

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,11 +1,3 @@
-provider "azurerm" {}
-
-# Make sure the resource group exists
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.product}-${var.component}-${var.env}"
-  location = "${var.location}"
-}
-
 provider "vault" {
   address = "https://vault.reform.hmcts.net:6200"
 }
@@ -163,7 +155,7 @@ module "key-vault" {
   env                 = "${var.env}"
   tenant_id           = "${var.tenant_id}"
   object_id           = "${var.jenkins_AAD_objectId}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  resource_group_name = "${module.s2s-api.resource_group_name}"
   # dcd_cc-dev group object ID
   product_group_object_id = "38f9dea6-e861-4a50-9e73-21e64f563537"
 }
