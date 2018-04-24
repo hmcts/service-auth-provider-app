8f134e1ed388a2de0c70640bd95bd904a5165375
Rafal Kondratowicz <rkondr@gmail.com>
2018-04-24 11:41:56 +0100

    Add non happy path test

--- api/src/functionalTest/java/uk/gov/hmcts/auth/provider/service/api/GetServiceDetailsTest.java
+++ api/src/functionalTest/java/uk/gov/hmcts/auth/provider/service/api/GetServiceDetailsTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.auth.provider.service.api;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.Test;
 import uk.gov.hmcts.auth.provider.service.api.model.SignIn;
 
@@ -13,16 +14,29 @@ public class GetServiceDetailsTest extends BaseFunctionalTest {
 
         String jwt = signInWith(new SignIn(this.serviceName, otp(this.serviceSecret))).asString();
 
-        RestAssured
+        getDetailsForToken(jwt)
+            .then()
+            .assertThat()
+            .statusCode(200)
+            .body(equalTo(this.serviceName));
+    }
+
+    @Test
+    public void should_return_401_when_invalid_jwt_token_is_sent() throws Exception {
+        getDetailsForToken("clearly_not_a_valid_jwt_token")
+            .then()
+            .assertThat()
+            .statusCode(401);
+    }
+
+    private Response getDetailsForToken(String jwt) {
+        return RestAssured
             .given()
             .relaxedHTTPSValidation()
             .baseUri(this.testUrl)
             .header("Authorization", "Bearer " + jwt)
             .when()
             .get("/details")
-            .then()
-            .assertThat()
-            .statusCode(200)
-            .body(equalTo(this.serviceName));
+            .andReturn();
     }
 }
