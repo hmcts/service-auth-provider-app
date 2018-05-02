7e24e764ef9ef0f528f7a6723edebab272f275a1
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2018-05-02 11:42:53 +0100

    Add swagger (#45)

--- src/main/java/uk/gov/hmcts/auth/provider/service/api/model/SignInWithoutOtp.java
+++ src/main/java/uk/gov/hmcts/auth/provider/service/api/model/SignInWithoutOtp.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.auth.provider.service.api.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import java.io.Serializable;
@@ -10,6 +11,7 @@ public class SignInWithoutOtp implements Serializable {
     private static final long serialVersionUID = 7053616822958826331L;
 
     @NotEmpty
+    @ApiModelProperty("Name of the service")
     public final String microservice;
 
     public SignInWithoutOtp(
