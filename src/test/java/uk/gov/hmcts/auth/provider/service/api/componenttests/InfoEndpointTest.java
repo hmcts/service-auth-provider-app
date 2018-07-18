bb83836a2bb105432ee065c33be15b447589d9a2
Rafal Kondratowicz <rkondr@gmail.com>
2018-07-18 15:30:42 +0100

    Add info contributor for configured services

---
+++ src/test/java/uk/gov/hmcts/auth/provider/service/api/componenttests/InfoEndpointTest.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.auth.provider.service.api.componenttests;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class InfoEndpointTest extends ComponentTestBase {
+
+    @Test
+    public void should_return_number_of_configured_services() throws Exception {
+        mvc
+            .perform(get("/info"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath(
+                "$.services",
+                containsInAnyOrder("cmc", "divorce")) // see config for tests
+            );
+    }
+}
--- src/test/java/uk/gov/hmcts/auth/provider/service/api/microservice/MicroserviceRepositoryTest.java
+++ src/test/java/uk/gov/hmcts/auth/provider/service/api/microservice/MicroserviceRepositoryTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.auth.provider.service.api.microservice;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,4 +25,19 @@ public class MicroserviceRepositoryTest {
         assertThat(repository.findOne("iD")).isEqualToComparingFieldByField(new Microservice("id", "key"));
     }
 
+    @Test
+    public void getNames_should_names_of_configured_services() {
+
+        MicroserviceRepository repo =
+            new MicroserviceRepository(ImmutableMap.of(
+                "a", "1",
+                "b", "2",
+                "c", "3"
+            ));
+
+        assertThat(repo.getNames())
+            .containsExactlyInAnyOrderElementsOf(
+                ImmutableSet.of("a", "b", "c")
+            );
+    }
 }
