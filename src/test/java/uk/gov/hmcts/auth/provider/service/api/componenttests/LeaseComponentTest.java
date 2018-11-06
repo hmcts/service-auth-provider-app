b30399f2bd9fa07d783c4e916d6b27f683da80e3
Rafal Kondratowicz <rkondr@gmail.com>
2018-11-06 23:24:14 +0000

    Add tests

--- src/test/java/uk/gov/hmcts/auth/provider/service/api/componenttests/LeaseComponentTest.java
+++ src/test/java/uk/gov/hmcts/auth/provider/service/api/componenttests/LeaseComponentTest.java
@@ -4,10 +4,15 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Value;
-import uk.gov.hmcts.auth.provider.service.api.error.ErrorDto;
+import org.springframework.http.MediaType;
 import uk.gov.hmcts.auth.provider.service.api.auth.totp.TotpAuthenticator;
+import uk.gov.hmcts.auth.provider.service.api.error.ErrorDto;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.core.IsNot.not;
+import static org.springframework.http.HttpStatus.NOT_ACCEPTABLE;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class LeaseComponentTest extends ComponentTestBase {
 
@@ -52,5 +57,15 @@ public class LeaseComponentTest extends ComponentTestBase {
                 Claims claims = Jwts.parser().setSigningKey(jwtKey).parseClaimsJws(token).getBody();
                 assertThat(claims.getSubject()).isEqualTo("divorce");
             });
+    }
+
+    @Test
+    public void should_be_able_to_provide_text_plain_error_if_required_by_the_client() throws Exception {
+        mvc
+            .perform(post("/lease")
+                .content("invalid content to trigger error...")
+                .accept(MediaType.TEXT_PLAIN_VALUE)
+            )
+            .andExpect(status().is(not(NOT_ACCEPTABLE.value())));
     }
 }
