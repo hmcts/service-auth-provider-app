ea92946ec60af9aa051b4d74ff746ddcc087ad61
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2018-11-07 10:47:30 +0000

    Content negotiation in error handler (#100)

--- src/test/java/uk/gov/hmcts/auth/provider/service/api/componenttests/DetailsComponentTest.java
+++ src/test/java/uk/gov/hmcts/auth/provider/service/api/componenttests/DetailsComponentTest.java
@@ -3,13 +3,19 @@ package uk.gov.hmcts.auth.provider.service.api.componenttests;
 import com.google.common.io.BaseEncoding;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.impl.crypto.MacProvider;
-import java.util.Date;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import uk.gov.hmcts.auth.provider.service.api.auth.jwt.JwtHS512Tool;
 import uk.gov.hmcts.auth.provider.service.api.error.ErrorDto;
 
+import java.util.Date;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.core.IsNot.not;
+import static org.springframework.http.HttpStatus.NOT_ACCEPTABLE;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class DetailsComponentTest extends ComponentTestBase {
 
@@ -62,5 +68,15 @@ public class DetailsComponentTest extends ComponentTestBase {
             .given()
             .when().details("Bearer " + jwt)
             .then().subject(actual -> assertThat(actual).isEqualTo("divorce"));
+    }
+
+    @Test
+    public void should_be_able_to_provide_text_plain_error_if_required_by_the_client() throws Exception {
+        mvc
+            .perform(get("/details")
+                .content("invalid content to trigger error...")
+                .accept(MediaType.TEXT_PLAIN_VALUE)
+            )
+            .andExpect(status().is(not(NOT_ACCEPTABLE.value())));
     }
 }
