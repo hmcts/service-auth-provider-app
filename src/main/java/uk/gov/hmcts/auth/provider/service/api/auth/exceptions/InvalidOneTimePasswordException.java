87baebf1766c47ba78b286cd50ec1e8c6b3153b7
Kevin Cross <kevin.j.cross@gmail.com>
2018-12-18 16:20:01 +0000

    moving to logging library 4.0.0 (#115)

--- src/main/java/uk/gov/hmcts/auth/provider/service/api/auth/exceptions/InvalidOneTimePasswordException.java
+++ src/main/java/uk/gov/hmcts/auth/provider/service/api/auth/exceptions/InvalidOneTimePasswordException.java
@@ -1,15 +1,4 @@
 package uk.gov.hmcts.auth.provider.service.api.auth.exceptions;
 
-import uk.gov.hmcts.reform.logging.exception.AlertLevel;
-import uk.gov.hmcts.reform.logging.exception.UnknownErrorCodeException;
-
-/**
- * SonarQube reports as error. Max allowed - 5 parents
- */
-@SuppressWarnings("squid:MaximumInheritanceDepth")
-public class InvalidOneTimePasswordException extends UnknownErrorCodeException {
-
-    public InvalidOneTimePasswordException() {
-        super(AlertLevel.P4, (Throwable) null);
-    }
+public class InvalidOneTimePasswordException extends RuntimeException {
 }
--- src/main/java/uk/gov/hmcts/auth/provider/service/api/auth/exceptions/TokenSignatureException.java
+++ src/main/java/uk/gov/hmcts/auth/provider/service/api/auth/exceptions/TokenSignatureException.java
@@ -1,15 +1,7 @@
 package uk.gov.hmcts.auth.provider.service.api.auth.exceptions;
 
-import uk.gov.hmcts.reform.logging.exception.AlertLevel;
-import uk.gov.hmcts.reform.logging.exception.UnknownErrorCodeException;
-
-/**
- * SonarQube reports as error. Max allowed - 5 parents
- */
-@SuppressWarnings("squid:MaximumInheritanceDepth")
-public class TokenSignatureException extends UnknownErrorCodeException {
-
-    public TokenSignatureException(Throwable cause) {
-        super(AlertLevel.P4, cause);
+public class TokenSignatureException extends RuntimeException {
+    public TokenSignatureException( Throwable e ){
+        super(e);
     }
 }
