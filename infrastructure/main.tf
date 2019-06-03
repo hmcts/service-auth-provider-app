e82fb8a4f6affabdd709c26660c924b866c8ce13
Javier Carretero <javi_1986@hotmail.com>
2019-06-03 12:10:29 +0100

    Added ethos-repl-service as microservicekey (#144)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -69,6 +69,7 @@ locals {
     "DG_DOCASSEMBLY_API"          = "microservicekey-dg-docassembly-api"
     "RD_PROFESSIONAL_API"         = "microservicekey-rd-professional-api"
     "RD_USER_PROFILE_API"         = "microservicekey-rd-user-profile-api"
+    "ETHOS_REPL_SERVICE"          = "microservicekey-ethos-repl-service"
   }
 
   microservice_secret_names = "${values(local.microservice_key_names)}"
