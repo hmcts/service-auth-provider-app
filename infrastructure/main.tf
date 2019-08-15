f3fd1cd16e25db77e1b96ee67eb83dfa1ed032b8
Tim Jacomb <tim.jacomb@hmcts.net>
2019-08-15 14:18:03 +0100

    Add MI identity (#177)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -131,4 +131,6 @@ module "key-vault" {
   # dcd_reform_dev_logs group object ID
   product_group_object_id = "70de400b-4f47-4f25-a4f0-45e1ee4e4ae3"
   common_tags             = "${var.common_tags}"
+
+  managed_identity_object_id = "${var.managed_identity_object_id}"
 }
