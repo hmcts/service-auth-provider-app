8259e9517033523a7e0932ef9e91b6614319a271
luigibk <41112628+luigibk@users.noreply.github.com>
2020-09-10 15:58:11 +0100

    Upgrade terraform. Migrate to v2 keyvault module (#315)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,26 +1,34 @@
-provider "azurerm" {
-  version = "1.44.0"
+provider azurerm {
+  features {}
 }
 
 locals {
-  vault_uri         = "${module.key-vault.key_vault_uri}"
-  vault_name         = "${module.key-vault.key_vault_name}"
+  vault_uri         = module.key-vault.key_vault_uri
+  vault_name        = module.key-vault.key_vault_name
 }
 
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.component}-${var.env}"
-  location = "${var.location}"
+  location = var.location
 
-  tags = "${var.common_tags}"
+  tags = var.common_tags
 }
 
 resource "azurerm_application_insights" "appinsights" {
   name                = "${var.product}-${var.component}-appinsights-${var.env}"
-  location            = "${var.appinsights_location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  application_type    = "Web"
+  location            = var.appinsights_location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
 
-  tags = "${var.common_tags}"
+  tags = var.common_tags
+
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to appinsights as otherwise upgrading to the Azure provider 2.x
+      # destroys and re-creates this appinsights instance
+      application_type,
+    ]
+  }
 }
 
 data "azurerm_user_assigned_identity" "rpe-shared-identity" {
@@ -29,22 +37,22 @@ data "azurerm_user_assigned_identity" "rpe-shared-identity" {
 }
 
 module "key-vault" {
-  source              = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  source              = "git@github.com:hmcts/cnp-module-key-vault?ref=azurermv2"
   product             = "s2s"
-  env                 = "${var.env}"
-  tenant_id           = "${var.tenant_id}"
-  object_id           = "${var.jenkins_AAD_objectId}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  env                 = var.env
+  tenant_id           = var.tenant_id
+  object_id           = var.jenkins_AAD_objectId
+  resource_group_name = azurerm_resource_group.rg.name
 
   # dcd_reform_dev_logs group object ID
   product_group_object_id = "70de400b-4f47-4f25-a4f0-45e1ee4e4ae3"
-  common_tags             = "${var.common_tags}"
+  common_tags             = var.common_tags
 
-  managed_identity_object_ids = ["${data.azurerm_user_assigned_identity.rpe-shared-identity.principal_id}"]
+  managed_identity_object_ids = [data.azurerm_user_assigned_identity.rpe-shared-identity.principal_id]
 }
 
 resource "azurerm_key_vault_secret" "AZURE_APPINSGHTS_KEY" {
   name         = "AppInsightsInstrumentationKey"
-  value        = "${azurerm_application_insights.appinsights.instrumentation_key}"
-  key_vault_id = "${module.key-vault.key_vault_id}"
+  value        = azurerm_application_insights.appinsights.instrumentation_key
+  key_vault_id = module.key-vault.key_vault_id
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,37 +1,30 @@
 variable "product" {
-  type = "string"
 }
 
 variable "component" {
-  type = "string"
 }
 
 variable "location" {
-  type    = "string"
   default = "UK South"
 }
 
 variable "env" {
-  type = "string"
 }
 
 variable "common_tags" {
-  type = "map"
+  type = map(string)
 }
 
 variable "tenant_id" {}
 
 variable "jenkins_AAD_objectId" {
-  type        = "string"
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
 variable "subscription" {
-  type = "string"
 }
 
 variable "appinsights_location" {
-  type        = "string"
   default     = "West Europe"
   description = "Location for Application Insights"
 }
