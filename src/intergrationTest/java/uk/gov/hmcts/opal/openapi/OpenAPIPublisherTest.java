eeae36a7d7636f6ffadaa27b0cfb8babd3bac634
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2024-08-19 11:56:33 +0100

    open api test

---
+++ src/intergrationTest/java/uk/gov/hmcts/opal/openapi/OpenAPIPublisherTest.java
@@ -0,0 +1,42 @@
+package uk.gov.hmcts.opal.openapi;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.hmcts.opal.BaseIntegrationTest;
+
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Built-in feature which saves service's swagger specs in temporary directory.
+ * Each CI run on master should automatically save and upload (if updated) documentation.
+ */
+@AutoConfigureMockMvc
+class OpenAPIPublisherTest extends BaseIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @DisplayName("Generate swagger documentation")
+    @Test
+    @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+    void generateDocs() throws Exception {
+        byte[] specs = mvc.perform(get("/v3/api-docs"))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsByteArray();
+
+        try (OutputStream outputStream = Files.newOutputStream(Paths.get("/tmp/openapi-specs.json"))) {
+            outputStream.write(specs);
+        }
+
+    }
+}
