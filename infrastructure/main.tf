7c55142272b5a24dad0a71e13174f871b0a04d0f
Kiran Kumar Yenigala <51748133+kiran-yenigala-hmcts@users.noreply.github.com>
2026-04-14 16:37:41 +0100

    CME-910: upgrade azurerm to 4.0 (#1114)
    * CME-910: upgrade azurerm to 4.0
    
    * CME-910: update ACR repos
    
    * Bumping chart version/ fixing aliases
    
    * CME-910: update ACR repos
    
    * CME-910: update ACR repos
    
    * pin to 4.x as per other hmcts repos
    
    ---------
    
    Co-authored-by: hmcts-jenkins-j-to-z <61242337+hmcts-jenkins-j-to-z[bot]@users.noreply.github.com>
    Co-authored-by: Dinesh Patel <dinesh1.patel@btinternet.com>

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,7 +15,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "application_insights" {
-  source = "git@github.com:hmcts/terraform-module-application-insights?ref=main"
+  source = "git@github.com:hmcts/terraform-module-application-insights?ref=4.x"
 
   env      = var.env
   product  = var.product
@@ -23,6 +23,7 @@ module "application_insights" {
   location = var.appinsights_location
 
   resource_group_name = azurerm_resource_group.rg.name
+  alert_limit_reached = true
 
   common_tags = var.common_tags
 }
