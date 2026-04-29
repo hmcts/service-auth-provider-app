f62b93cd5e6338551f78a378749491a6ee460b36
Ieuan Byers <144245358+ieuanb74@users.noreply.github.com>
2026-04-29 09:10:39 +0100

    Grant new jenkins MIs access to KVs (#1116)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -45,6 +45,7 @@ module "key-vault" {
   env                 = var.env
   tenant_id           = var.tenant_id
   object_id           = var.jenkins_AAD_objectId
+  jenkins_object_id   = data.azurerm_user_assigned_identity.jenkins.principal_id
   resource_group_name = azurerm_resource_group.rg.name
 
   # dcd_reform_dev_logs group object ID
@@ -52,6 +53,11 @@ module "key-vault" {
   common_tags             = var.common_tags
 
   managed_identity_object_ids = [data.azurerm_user_assigned_identity.rpe-shared-identity.principal_id]
+}
+
+data "azurerm_user_assigned_identity" "jenkins" {
+  name                = "jenkins-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
 }
 
 resource "azurerm_key_vault_secret" "connection-string" {
